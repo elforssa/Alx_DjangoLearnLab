44caeda65801cb58545423f038ceed2629d9ecff
Elforssae <elforssa@gmail.com>
2024-08-19 20:19:09 +0200

    5try

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,16 +5,12 @@ from django.shortcuts import render
 
 # relationship_app/views.py
 
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
-
-# relationship_app/views.py
-
-from django.views.generic import DetailView
-from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library

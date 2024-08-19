10a408c5ec4ae584713ae28853241ec1305a1972
Elforssae <elforssa@gmail.com>
2024-08-19 20:06:00 +0200

    3try

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# relationship_app/views.py
+
 from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    # Create a simple text list of books and their authors
-    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(book_list, content_type="text/plain")
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# relationship_app/views.py
 
 from django.views.generic import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-

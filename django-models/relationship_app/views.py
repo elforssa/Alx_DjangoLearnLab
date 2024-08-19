c981ab1cbbaa8b2b3dff55487dca00b60f836507
Elforssae <elforssa@gmail.com>
2024-08-19 20:01:20 +0200

    2try

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,4 +5,15 @@ from .models import Book
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    # Create a simple text list of books and their authors
+    book_list = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(book_list, content_type="text/plain")
+
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+

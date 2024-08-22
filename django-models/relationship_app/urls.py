f1a0349f5e545b9e34d9efca712e6fe10494ea3f
Elforssae <elforssa@gmail.com>
2024-08-22 15:25:22 +0200

    1try

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,3 +14,13 @@ urlpatterns = [
     path('', include('relationship_app.urls')),  # Include the relationship_app URLs
 ]
 
+# relationship_app/urls.py
+
+from django.urls import path
+from .views import CustomLoginView, CustomLogoutView, RegisterView
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -13,3 +13,28 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
+
+
+# relationship_app/views.py
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
+# Login View
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+# Logout View
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
+
+# Registration View
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')  # Redirect to login after registration

95e77eb8ac5e0ae111b4da10d3026e219421fc16
Elforssae <elforssa@gmail.com>
2024-08-22 15:32:34 +0200

    4try

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -24,3 +24,7 @@ urlpatterns = [
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
+
+views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name=
+                                                                         
+                                                                         

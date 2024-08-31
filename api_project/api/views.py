f2d2ccfbe21feb8e88a8441a83dbd2e436dec57f
Elforssae <elforssa@gmail.com>
2024-08-31 16:40:53 +0200

    rst 1try

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()  # Retrieve all books
+    serializer_class = BookSerializer  # Use the BookSerializer to convert Book objects to JSON

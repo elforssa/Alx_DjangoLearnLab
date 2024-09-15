39a9651e21cdc5cce1e6c91430a98042cb8d9ba2
Elforssae <elforssa@gmail.com>
2024-09-15 17:41:23 +0200

    1stry

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -10,3 +10,37 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f"/posts/{self.id}/"
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Post(models.Model):
+    # Existing fields
+    pass
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author} on {self.post}'
